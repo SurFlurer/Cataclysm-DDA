7104ae8f86f0da79ed9df18925e01b5810c34c13
Surflurer <22912139+Surflurer@users.noreply.github.com>
2025-01-17 20:24:07 +0800

    make build small

--- build-scripts/windist.ps1
+++ build-scripts/windist.ps1
@@ -4,8 +4,8 @@ if (Test-path bindist) {
 
 mkdir bindist
 cp cataclysm-tiles.exe bindist/cataclysm-tiles.exe
-cp cataclysm-tiles.stripped.pdb bindist/cataclysm-tiles.pdb
-cp tools/format/json_formatter.exe bindist/json_formatter.exe
+#cp cataclysm-tiles.stripped.pdb bindist/cataclysm-tiles.pdb
+#cp tools/format/json_formatter.exe bindist/json_formatter.exe
 
 mkdir bindist/lang
 cp -r lang/mo bindist/lang
